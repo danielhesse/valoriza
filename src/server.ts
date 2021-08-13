7b8f3ece07cdb1b2e9f7b21e73bcc15c9bcb9d68
Daniel Hessel <hesseldaniel21@gmail.com>
2021-08-13 09:46:32 -0300

    feat: exception handling

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
 
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+
 import { routes } from "./routes";
 
 import "./database";
@@ -10,6 +12,19 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use((error: Error, request: Request, response: Response, _: NextFunction) => {
+  if (error instanceof Error) {
+    return response.status(400).json({
+      error: error.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  });
+});
 
 app.listen(process.env.port || 3333, () => {
   console.log("🚀 Server running on port 3333.");
